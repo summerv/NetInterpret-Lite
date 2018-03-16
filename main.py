4c8f045c96e6c74686efff731c1d97c47d0699d5
vicky <taojie@pku.edu.cn>
2018-03-16 23:28:19 +1100

    add seleted decision tree

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ model2 = loadmodel(places365_val_exp.hook_feature)
 features, maxfeature = fo.feature_extraction(model=model)
 
 ########### STEP 1.5: places365_val featre extraction ###########
-places365_features, true_index, images_365 = pfe.feature_extraction(model=model2)
+places365_features, true_index, images_365, predicted_label = pfe.feature_extraction(model=model2)
 
 for layer_id, layer in enumerate(settings.FEATURE_NAMES):
     ############ STEP 2: calculating threshold ############
@@ -39,7 +39,7 @@ for layer_id, layer in enumerate(settings.FEATURE_NAMES):
 
     ########### STEP5: experiment about whether images activate more on units which have meanings ########
     # activate_experiment.activate_exp_broden(fo.data, features[layer_id], thresholds, rendered_order)
-    activate_info, dt_features, true_images, feature_names = activate_experiment.activate_exp_places365(images_365[:100], true_index[:100], places365_features[layer_id][:100], thresholds, rendered_order)
+    dt_features, true_images, feature_names = activate_experiment.activate_exp_places365(images_365, true_index, places365_features[layer_id], thresholds, rendered_order)
     activate_experiment.gen_gbdt(dt_features, true_images, feature_names)
     if settings.CLEAN:
         clean()
